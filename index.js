8f0996e5e1e4d787fcba6f89117f1d8574f1633d
dscottyoung2022 <131805518+dscottyoung2022@users.noreply.github.com>
2024-07-11 12:51:37 -0400

    Added Moment js integration
    For timezone, time and date for selected cities and current cities shown.

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
-alert("Hello");
+function updateTime() {
+  // Boston
+  let bostonElement = document.querySelector("#boston");
+
+  if (bostonElement) {
+    let bostonDateElement = bostonElement.querySelector(".date");
+    let bostonTimeElement = bostonElement.querySelector(".time");
+    let bostonTime = moment().tz("America/New_York");
+
+    bostonDateElement.innerHTML = bostonTime.format("MMMM Do YYYY");
+    bostonTimeElement.innerHTML = bostonTime.format(
+      "hh:mm:ss [<small>]A[</small>]"
+    );
+  }
+
+  //Tokyo
+  let tokyoElement = document.querySelector("#tokyo");
+
+  if (tokyoElement) {
+    let tokyoDateElement = tokyoElement.querySelector(".date");
+    let tokyoTimeElement = tokyoElement.querySelector(".time");
+    let tokyoTime = moment().tz("Asia/Tokyo");
+
+    tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
+    tokyoTimeElement.innerHTML = tokyoTime.format(
+      "hh:mm:ss [<small>]A[</small>]"
+    );
+  }
+}
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
+  </div>
+  `;
+}
+
+updateTime();
+setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
